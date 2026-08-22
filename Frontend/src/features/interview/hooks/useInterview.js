450a1bba7674bc3f09d1d41185d048530d9fd01b
Vaibhvee Prakash <vaibhveeprakash25@gmai.com>
2026-08-23 03:54:59 +0530

    Add interview feature

---
+++ Frontend/src/features/interview/hooks/useInterview.js
@@ -0,0 +1,109 @@
+import {getInterviewById, getAllInterviewReports, generateInterviewReport, generateResumePdf} from "../services/interview.api"
+import { useContext, useEffect } from "react"
+import { InterviewContext } from "../interview.context"
+import { useParams } from "react-router"
+
+export const useInterview = () => {
+
+    const context = useContext(InterviewContext)
+
+    if (!context) {
+        throw new Error(
+            "useInterview must be used within an InterviewProvider"
+        )
+    }
+
+    const {loading, setLoading, report, setReport, reports, setReports} = context
+
+    const { interviewId } = useParams()
+
+
+    const generateReport = async ({jobDescription,selfDescription,resumeFile}) => {
+    setLoading(true)
+    try {
+        const response = await generateInterviewReport({
+            jobDescription,
+            selfDescription,
+            resumeFile
+        })
+
+        setReport(response.interviewReport)
+
+        return response.interviewReport
+
+    } catch (error) {
+
+        console.log(error)
+
+    } finally {
+
+        setLoading(false)
+
+    }
+}
+
+
+    const getReportById = async (interviewId) => {
+
+        setLoading(true)
+
+        try {
+
+            const response = await getInterviewById(interviewId)
+
+            setReport(response.interviewReport)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+
+        }
+    }
+
+
+    const getReports = async () => {
+
+        setLoading(true)
+        try {
+            const response = await getAllInterviewReports()
+            setReports(response.interviewReports)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+
+    const getResumePdf = async (interviewReportId) => {
+    setLoading(true)
+    let response = null
+    try {
+        response = await generateResumePdf(interviewReportId)
+        const url = window.URL.createObjectURL(new Blob([response], { type: "application/pdf" }))
+        const link = document.createElement("a")
+        link.href = url
+        link.setAttribute("download", `resume_${interviewReportId}.pdf`)
+        document.body.appendChild(link)
+        link.click()
+        link.remove()
+        window.URL.revokeObjectURL(url)
+    } catch (err) {
+        console.error("Failed to download resume PDF:", err)
+    } finally {
+        setLoading(false)
+    }
+}
+
+    useEffect(() => {
+
+        if (interviewId) {
+            getReportById(interviewId)
+        } else {
+            getReports()
+        }
+    }, [interviewId])
+
+
+    return {loading,report,reports,generateReport,getReportById,getReports}
+}
---
+++ Frontend/src/features/interview/services/interview.api.js
@@ -0,0 +1,72 @@
+import axios from "axios"
+
+const api = axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials: true,
+})
+
+/**
+ * Service to generate interview report based on user self description,
+ * resume and job description.
+ */
+export const generateInterviewReport = async ({
+    jobDescription,
+    selfDescription,
+    resumeFile
+}) => {
+    const formData = new FormData()
+
+    formData.append("jobDescription", jobDescription)
+    formData.append("selfDescription", selfDescription)
+
+    if (resumeFile) {
+        formData.append("resume", resumeFile)
+    }
+
+    const response = await api.post(
+        "/api/interview/",
+        formData,
+        {
+            headers: {
+                "Content-Type": "multipart/form-data"
+            }
+        }
+    )
+
+    return response.data
+}
+
+/**
+ * Service to get interview report by interviewId.
+ */
+export const getInterviewById = async (interviewId) => {
+    const response = await api.get(
+        `/api/interview/report/${interviewId}`
+    )
+
+    return response.data
+}
+
+/**
+ * Service to get interview reports of logged in user.
+ */
+export const getAllInterviewReports = async () => {
+    const response = await api.get(
+        "/api/interview"
+    )
+
+    return response.data
+}
+
+/**
+ * Service to generate resume PDF.
+ */
+export const generateResumePdf = async (interviewReportId) => {
+    const response = await api.post(
+        `/api/interview/resume/pdf/${interviewReportId}`,
+        null,{
+            responseType: "blob"
+        })
+        
+    return response.data
+}
