ac78175b6da8b826f45bd51bf05b08f30934ec7b
Vaibhvee Prakash <vaibhveeprakash25@gmai.com>
2026-08-25 01:39:28 +0530

    Fix authentication cookies for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -48,8 +48,8 @@ async function registerUserController(req, res) {
 
     res.cookie("jwt_token", token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000
     })
 
